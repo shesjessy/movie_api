1656f5c010aede03d5473b68c40c32fae691b9e6
shesjessy <benitezjessicae@gmail.com>
2024-08-11 13:32:28 -0700

    Exercise 2.5 Task

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 // Use Morgan to log all requests
 app.use(morgan("common"));
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
@@ -55,6 +58,69 @@ app.get("/movies", (req, res) => {
 // Define a route to handle the root URL
 app.get("/", (req, res) => {
     res.send("Welcome to My Movie API!");
+});
+
+// Additional Routes
+
+// Return data about a single movie by title to the user
+app.get("/movies/:title", (req, res) => {
+    const title = req.params.title;
+    res.send(
+        `Successful GET request returning data about the movie titled: ${title}`
+    );
+});
+
+// Return data about a genre by name/title
+app.get("/genres/:name", (req, res) => {
+    const genreName = req.params.name;
+    res.send(
+        `Successful GET request returning data about the genre: ${genreName}`
+    );
+});
+
+// Return data about a director by name
+app.get("/directors/:name", (req, res) => {
+    const directorName = req.params.name;
+    res.send(
+        `Successful GET request returning data about the director: ${directorName}`
+    );
+});
+
+// Allow new users to register
+app.post("/users", (req, res) => {
+    res.send("POST request to register a new user");
+});
+
+// Allow users to update their user info (username)
+app.put("/users/:username", (req, res) => {
+    const username = req.params.username;
+    res.send(
+        `Successful PUT request to update user info for username: ${username}`
+    );
+});
+
+// Allow users to add a movie to their list of favorites
+app.post("/users/:username/movies/:movieID", (req, res) => {
+    const { username, movieID } = req.params;
+    res.send(
+        `Successful POST request to add movie with ID ${movieID} to ${username}'s list of favorites`
+    );
+});
+
+// Allow users to remove a movie from their list of favorites
+app.delete("/users/:username/movies/:movieID", (req, res) => {
+    const { username, movieID } = req.params;
+    res.send(
+        `Successful DELETE request to remove movie with ID ${movieID} from ${username}'s list of favorites`
+    );
+});
+
+// Allow existing users to deregister
+app.delete("/users/:username", (req, res) => {
+    const username = req.params.username;
+    res.send(
+        `Successful DELETE request to deregister user with username: ${username}`
+    );
 });
 
 // Error-handling middleware function
